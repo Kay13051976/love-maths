bc475487d691330c509e563ffb8c849fbcc5ae61
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2023-07-08 17:01:11 +0000

    Add code to increment score and incorect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,9 +42,12 @@ function checkAnswer() {
 
   if (isCorrect) {
     alert("Hey! You got it right :D");
-  }else {
-    alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
-
+    incrementScore();
+  } else {
+    alert(
+      `Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`
+    );
+    incrementWrongAnswer();
   }
   runGame(calculatedAnswer[1]);
 }
@@ -65,8 +68,20 @@ function calculateCorrectAnswer() {
     throw `Unimplemented operator ${operator}.Aborting!`;
   }
 }
-function incrementScore() {}
-function incrementWrongAnswer() {}
+/**
+ * Gets the current score form the DOM and increments it by 1
+ */
+function incrementScore() {
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = ++oldScore;
+}
+/**
+ * Gets the current tally of incorrect answers form the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+  let oldScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++oldScore;
+}
 function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
